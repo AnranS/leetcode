b5944ac0d886fe35ae9fc2ea886cb7f68a5bce42
gaopeng12 <gaopeng12@58.com>
2020-06-05 09:10:38 +0800

    add:leetcode152`乘积最大的子数组`

---
+++ js_leetcode/medium/problem152/problem152.ts
@@ -0,0 +1,18 @@
+function maxProduct(nums: number[]): number {
+    const maxProductMemo = new Array<number>();
+    const minProductMemo = new Array<number>();
+    maxProductMemo[0] = nums[0];
+    minProductMemo[0] = nums[0];
+
+    let max = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        maxProductMemo[i] = Math.max(nums[i], nums[i] * maxProductMemo[i - 1], nums[i] * minProductMemo[i - 1]);
+        minProductMemo[i] = Math.min(nums[i], nums[i] * maxProductMemo[i - 1], nums[i] * minProductMemo[i - 1]);
+
+        max = Math.max(maxProductMemo[i], max);
+    }
+    return max;
+};
+
+let arrnums: number[] = [-2];
+console.log(maxProduct(arrnums));
