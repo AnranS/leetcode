89e0a92e40395a89cb4fcbc90c89bc1d9a07657e
gaopeng12 <gaopeng12@58.com>
2020-11-11 18:45:59 +0800

    add:剑指offer03

---
+++ js_leetcode/sword_offer/problem03/index.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findRepeatNumber = function(nums) {
+    let map = new Map()
+
+    for(let i = 0;i<nums.length;i++) {
+        if(map.has(nums[i])) {
+            return nums[i];
+        } else {
+            map.set(nums[i], 1);
+        }
+    }
+};
