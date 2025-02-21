0c01452404c2500775319fc8af682d122d95be0f
gaopeng.anran <gaopeng.anran@bytedance.com>
2025-02-21 15:39:31 +0800

    add 2516

---
+++ medium/2516.py
@@ -0,0 +1,16 @@
+from typing import Counter
+
+
+class Solution:
+    def takeCharacters(self, s: str, k: int) -> int:
+        cnt = Counter(s)
+        if any(cnt[c] < k for c in "abc"):
+            return -1
+        mx = left = 0
+        for right, c in enumerate(s):
+            cnt[c] -= 1
+            while cnt[c] < k:
+                cnt[s[left]] += 1
+                left += 1
+            mx = max(mx, right - left + 1)
+        return len(s) - mx
