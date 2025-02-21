c501da6715f4e1c5b8a59e3e97947b9abb192d87
gaopeng.anran <gaopeng.anran@bytedance.com>
2025-02-21 08:47:48 +0800

    add 1695

---
+++ medium/1695.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        cnt = defaultdict(int)
+        ans = left = 0
+        n = len(nums)
+        s = 0
+        for right in range(n):
+            in_ = nums[right]
+            cnt[in_] += 1
+            s += in_
+            if cnt[in_] == 1:
+                ans = max(ans, s)
+            else:
+                while cnt[in_] != 1:
+                    out_ = nums[left]
+                    cnt[out_] -= 1
+                    left += 1
+                    s -= out_
+        return ans
+        
+r = Solution().maximumUniqueSubarray([4,2,4,5,6])
+
+print(r)
